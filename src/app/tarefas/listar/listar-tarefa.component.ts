0e7cee5fea460b81eb254563e1fcba3b54dea589
Misserine C <misserine.carvajal@gmail.com>
2023-07-19 21:55:02 -0300

    :sparkles: Finalizado componente cadastrar-tarefa

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -14,15 +14,6 @@ export class ListarTarefaComponent implements OnInit {
 
     ngOnInit() {
         this.tarefas = this.listarTodos();
-        this.tarefas = [
-            new Tarefa(1, 'Tarefa 1', true),
-            new Tarefa(2, 'Tarefa 2', false),
-            new Tarefa(3, 'Tarefa 3', true),
-            new Tarefa(4, 'Tarefa 4', false),
-            new Tarefa(5, 'Tarefa 5', true),
-            new Tarefa(6, 'Tarefa 6', true),
-            new Tarefa(7, 'Tarefa 7', false),
-        ];
     }
 
     listarTodos(): Tarefa[] {
